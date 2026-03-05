cca53b00d8f7997374757ee9c09303af29e68bef
Guillaume <guillaume@noeupapp.com>
2026-03-05 22:09:43 +0100

    Ajout thème enfant AFLA (Astra)

---
+++ wp-content/themes/afla/functions.php
@@ -0,0 +1,6 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'afla_enqueue_styles' );
+function afla_enqueue_styles() {
+    wp_enqueue_style( 'astra-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'afla-style', get_stylesheet_uri(), array( 'astra-style' ), '1.0.0' );
+}
